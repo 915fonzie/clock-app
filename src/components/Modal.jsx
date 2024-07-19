f138f110f29530da6d6017ac3cfcd4e86957cec1
Alfonso Pruneda <alfonso.pru@outlook.com>
2024-07-19 16:13:06 -0600

    refactored code

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -5,10 +5,6 @@ export default function Modal({ data, hour, className }) {
     const { timezone, day_of_year, day_of_week, week_number } = data
     const modalRef = React.useRef(null)
 
-    React.useEffect(() => {
-
-     }, [])
-
     if (hour >= 5 || hour < 18) {
         if (modalRef.current) {
             modalRef.current.classList.add('day-modal')
@@ -30,11 +26,12 @@ export default function Modal({ data, hour, className }) {
                 <h6 className='item-2'>day of the year</h6>
                 <h6 className='item-3'>day of the week</h6>
                 <h6 className='item-4'>week number</h6>
+
                 <h2 className='item-5'>{timezone}</h2>
                 <h2 className='item-6'>{day_of_year}</h2>
                 <h2 className='item-7'>{day_of_week}</h2>
                 <h2 className='item-8'>{week_number}</h2>
-                <div className='line-break' aria-label='line break'></div>
+                <div className='line-break'></div>
             </div>
         </div>
     )
